4c4eefdefff13bdeeafb97e169fcd958024e3f8c
raman <seleniumcoaching@gmail.com>
2014-06-24 00:24:55 +0530

    Initial Commit

---
+++ src/test/java/LoginTest.java
@@ -0,0 +1,38 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+
+public class LoginTest {
+	
+	
+	public static WebDriver driver;
+	
+	@BeforeSuite
+	public void setUp(){
+		
+		driver = new FirefoxDriver();
+		
+		
+		
+	}
+
+	@Test
+	public void doLogin(){
+		
+		
+		driver.get("http://gmail.com");
+		driver.findElement(By.id("Email")).sendKeys("trainer@way2automation.com");
+		driver.findElement(By.id("Passwd")).sendKeys("sdfdsf");
+	}
+	
+	@AfterSuite
+	public void tearDown(){
+		
+		driver.quit();
+	}
+	
+}
