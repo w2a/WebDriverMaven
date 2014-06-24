33f369c71195a64363133dcebd9db3cb67513ca6
raman <seleniumcoaching@gmail.com>
2014-06-24 17:33:54 +0530

    Committing for Jenkins2

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -27,7 +27,11 @@ public class LoginTest {
 		driver.get("http://gmail.com");
 		driver.findElement(By.id("Email")).sendKeys("trainer@way2automation.com");
 		driver.findElement(By.id("Passwd")).sendKeys("sdfdsf");
-		
+		/*
+		 * 
+		 * 
+		 * 
+		 */
 	
 	
 	}
